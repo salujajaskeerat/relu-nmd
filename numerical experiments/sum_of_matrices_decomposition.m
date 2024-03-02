7cdc67905c992235e38c82d9d207ddc76e304c2a
Jaskeerat Singh Saluja <salujajaskeerat@gmail.com>
2024-03-02 17:38:05 +0530

    general matrix decomposition

--- numerical experiments/sum_of_matrices_decomposition.m
+++ numerical experiments/sum_of_matrices_decomposition.m
@@ -11,19 +11,19 @@ clc
 rng(2023)
 
 
-m=400;
-n=200;
-X=randn(m,n);
+m=1000;
+n=1000;
+X=randi([-1000,1000],m,n);
 
 fprintf('rank of x = %d\n',[rank(X)]);
 
 % Parameters
-r=128; %rank
-fprintf('r set to %d\n',[r]);
+%r=512; %rank
+
 
 param=struct();
 %Parameters setting
-param.maxit=30000000; param.tol=1.e-4; param.tolerr = 0; param.time=20;
+param.maxit=1000; param.tol=1.e-4; param.tolerr = 0; param.time=20;
 %A-NMD parameters
 param.beta=0.7; param.eta=0.4; param.gamma=1.1; param.gamma_bar=1.05;
 %3 Blocks momentum parameters
@@ -39,10 +39,10 @@ param.alpha=0.6;
 Xplus= max(0,X);
 Xminus=max(0,-X);
 
-fprintf('rankf of Xplus= %d and Xminus= %d ',[rank(Xplus),rank(Xminus)]);
+fprintf('rankf of Xplus= %d and Xminus= %d\n ',[rank(Xplus),rank(Xminus)]);
 
 % Nucleur norma initialization
-param.Theta0=Xplus;
+% param.Theta0=Xplus;
 
 
 %Set the interval for cubic spline interpolation for the average error
@@ -50,52 +50,41 @@ c=100;
 time=linspace(0,param.time,c);
 
 
-rep=1;
-for k=1:rep
-    %Naive with Andersens' acceleration
-    [T_ANMD,err_ANMD,it_ANMD,t_ANMD]=A_NMD(Xplus,r/2,param);
-    err_ANMD_k(k)=err_ANMD(end);                     %save final error
-    p_ANMD(:,k) = spline(t_ANMD,err_ANMD,time); %interpolate in the desired points (needed to compute the average)
+normX=norm(X,'fro');
 
+
+log_rank=  int64(floor(log2(rank(X))));
+for i=log_rank-2:log_rank
+    r=2^i;
+    fprintf('r set to %d\n',[r]);
+    rep=1;
+    for k=1:rep
+        [theta_XPLUS,err_XPLUS,i_XPLUS,time_XPLUS]=A_NMD(Xplus,r,param);
+        % for Xminus
+        [theta_XMINUS,err_XMINUS,i_XMINUS,time_XMINUS]=A_NMD(Xminus,r,param);
+    
+        % Compute err for original X
+        err_X= norm((X-max(0,theta_XPLUS)+max(0,theta_XMINUS)),'fro')/normX;
+    end
+    %Plot the error vs number of iteration
+    figure;
+    % Plot the data
+    x_range=linspace(1,max(size(err_XMINUS,2),size(err_XPLUS,2)),100);
+    plot(err_XPLUS, 'r-', 'LineWidth', 2); hold on;
+    plot(err_XMINUS, 'b-.', 'LineWidth', 2);
+    plot(x_range,err_X*ones(size(x_range)),'cyan-.','LineWidth',1.5);
+    plot(x_range,(min(err_XPLUS)+min(err_XMINUS))*ones(size(x_range)),'black--','LineWidth',1.5);
+    % Set axis labels and legend
+    xlabel('Time', 'FontSize', 18, 'FontName', 'Times');
+    ylabel('Relative Error', 'FontSize', 18, 'FontName', 'Times');
+    legend({'Err XPLUS', 'Err XMINUS','ERR X','final Err XPLUS + final Err XMINUS'}, 'FontSize', 18, 'FontName', 'Times');
+    % Set title with specific details
+    title(sprintf('Err XPLUS and XMINUS (r=%d)', r));
+    % Set log scale for the y-axis
+    set(gca, 'YScale', 'log');
+    % Adjust tick marks and grid lines
+    set(gca, 'FontSize', 14);
+    grid on;
+    % Add grid lines with minor ticks for better readability
+    grid minor;
 end
-
-
-%Find best solution
-opt=min(err_ANMD_k);
-
-%Compute the average error
-mean_ANMD=sum(p_ANMD')/rep;
-% mean_3B=sum(p_3B')/rep;
-% mean_AEM=sum(p_AEM')/rep;
-
-%Subtract best solution
-p_ANMD_mean=mean_ANMD-opt; 
-% p_3B_mean=mean_3B-opt; 
-% p_AEM_mean=mean_AEM-opt;
-
-%Plot the error vs number of iteration
-
-figure;
-
-% Plot the data
-plot(err_ANMD, 'r-', 'LineWidth', 2); hold on;
-% plot(err_3B(1:300), 'b-.', 'LineWidth', 2);
-% plot(err_AEM, 'g--', 'LineWidth', 2);
-
-% Set axis labels and legend
-xlabel('Time', 'FontSize', 18, 'FontName', 'Times');
-ylabel('Relative Error', 'FontSize', 18, 'FontName', 'Times');
-legend({'A-NMD', '3B-NMD', 'A-EM'}, 'FontSize', 18, 'FontName', 'Times');
-
-% Set title with specific details
-title(sprintf('X_plus and X_minus (r=%d)', r), 'FontSize', 20, 'FontName', 'Times');
-
-% Set log scale for the y-axis
-set(gca, 'YScale', 'log');
-
-% Adjust tick marks and grid lines
-set(gca, 'FontSize', 14);
-grid on;
-
-% Add grid lines with minor ticks for better readability
-grid minor;
